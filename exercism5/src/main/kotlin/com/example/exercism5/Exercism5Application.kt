1a06d035a2222e19f5a26d3b067b8498ef35d477
rubelsllan1 <118912229+rubelsllan1@users.noreply.github.com>
2023-06-21 10:33:07 -0500

    Add files via upload

---
+++ exercism5/src/main/kotlin/com/example/exercism5/Exercism5Application.kt
@@ -0,0 +1,11 @@
+package com.example.exercism5
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Exercism5Application
+
+fun main(args: Array<String>) {
+	runApplication<Exercism5Application>(*args)
+}
